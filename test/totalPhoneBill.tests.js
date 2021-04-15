030b72d8a510084581fef8600ef28f1091fdf8e0
GeniusSiz01 <sizwe.msimango01@gmail.com>
2021-04-15 12:27:18 +0200

    my commit after a more descriptive story

--- test/totalPhoneBill.tests.js
+++ test/totalPhoneBill.tests.js
@@ -1,6 +1,6 @@
 describe('Test my totalPhoneBill function' , function() {
 
-    it('It should return "2.75") ' , function(){
+    it('that takes in a string calls made and "sms,s" sent. Calculate the total bill for the data provided. there are 2 calls and 3 sms,s") ' , function() {
         
         assert.deepEqual(totalPhoneBill('l'), "R2.75");
     });
