517fdbf912c6eba7624c61107ade13e803686402
GeniusSiz01 <sizwe.msimango01@gmail.com>
2021-04-15 11:02:46 +0200

    my commit after a more descriptive story

--- test/yearsAgo.tests.js
+++ test/yearsAgo.tests.js
@@ -1,12 +1,12 @@
 describe('Test my yearsAgo function' , function() {
 
-    it('It should return how many years ago that year is from the current year. ' , function(){
+    it('that takes in a year and return how many years ago that year is from the current year.. ' , function(){
         
         assert.deepEqual(yearsAgo('2011'), 10);
 
     });
 
-    it('It should return how many years ago that year is from the current year. ' , function(){
+    it('that takes in a year and return how many years ago that year is from the current year.. ' , function(){
         
         assert.deepEqual(yearsAgo('2015'), 6);
 
