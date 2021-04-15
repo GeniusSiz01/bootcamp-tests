d3ac5f99023ab7e5b50c13c6c9651cbfa2a80953
GeniusSiz01 <sizwe.msimango01@gmail.com>
2021-04-15 10:12:50 +0200

    my commit after a more descriptive story

--- test/isFromBellville.tests.js
+++ test/isFromBellville.tests.js
@@ -3,12 +3,12 @@ console.log(isFromBellville("CA"))
 console.log(isFromBellville("DY"))*/
 
 describe('Test my isFromBellvile function' , function(){
-    it('It should only return "true" for "CY" ' , function(){
+    it('that returns "true" if a registration number is for Bellville otherwise returns "false". Registration numbers for Bellville starts with "CY". ' , function(){
         assert.equal(true,isFromBellville("CY"));
 
         /*assert.deepEqual([2,2],[2,2]);*/
     });
-    it('It should return "false" for  for anything outside of "CY" ' , function(){
+    it('that returns "true" if a registration number is for Bellville otherwise returns "false". Registration numbers for Bellville starts with "CY". ' , function(){
         assert.equal(false, isFromBellville("CA"));
 
         /*assert.deepEqual([2,2],[2,2]);*/
