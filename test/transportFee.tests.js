2151ffda61b8cd55eac2bb18a178f93beeedd449
GeniusSiz01 <sizwe.msimango01@gmail.com>
2021-04-15 12:18:19 +0200

    my commit after a more descriptive story

--- test/transportFee.tests.js
+++ test/transportFee.tests.js
@@ -1,16 +1,16 @@
 describe('Test my transportFee function' , function() {
 
-    it('It should return the right price based on the shift you are working "Morning=R20" ' , function(){
+    it('It should return the right price based on the shift you are working "Morning=R20" as your work provides transport back home. If you work "nightshift" you pay nothing, because your work provides transport from and to work. ' , function(){
         
         assert.deepEqual(transportFee('morning'), "R20");
     });
 
-    it('It should return the right price based on the shift you are working "Afternoon=R10" ' , function(){
+    it('It should return the right price based on the shift you are working "Morning=R20" as your work provides transport back home. If you work "nightshift" you pay nothing, because your work provides transport from and to work. ' , function(){
         
         assert.deepEqual(transportFee('afternoon'), "R10");
     });
 
-    it('It should return the right price based on the shift you are working "Nightshift=free" ' , function(){
+    it('It should return the right price based on the shift you are working "Morning=R20" as your work provides transport back home. If you work "nightshift" you pay nothing, because your work provides transport from and to work. ' , function(){
         
         assert.deepEqual(transportFee('nightshift'), "free");
     });
