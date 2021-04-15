f9825d94240c570216e25f63f4c4392a159ae65a
GeniusSiz01 <sizwe.msimango01@gmail.com>
2021-04-15 12:23:01 +0200

    my commit after a more descriptive story

--- test/fromWhere.tests.js
+++ test/fromWhere.tests.js
@@ -1,19 +1,19 @@
 describe('Test my fromWhere function', function () {
 
-    it('It should return the town the car is from) ', function () {
+    it('that takes a car registration number as a parameter and returns the town the car is from. If the reg number starts with CY return Bellville, for CJ return Paarl, for CA return Cape Town otherwise return Some other place!) ', function () {
 
         assert.deepEqual(fromWhere('CY'), "Bellville");
     });
-    it('It should return the town the car is from) ', function () {
+    it('that takes a car registration number as a parameter and returns the town the car is from. If the reg number starts with CY return Bellville, for CJ return Paarl, for CA return Cape Town otherwise return Some other place!) ', function () {
 
         assert.deepEqual(fromWhere('CA'), "Cape Town");
     });
 
-    it('It should return the town the car is from) ', function () {
+    it('that takes a car registration number as a parameter and returns the town the car is from. If the reg number starts with CY return Bellville, for CJ return Paarl, for CA return Cape Town otherwise return Some other place!) ', function () {
 
         assert.deepEqual(fromWhere('CJ'), "Paarl");
     });
-    it('It should return the town the car is from) ', function () {
+    it('that takes a car registration number as a parameter and returns the town the car is from. If the reg number starts with CY return Bellville, for CJ return Paarl, for CA return Cape Town otherwise return Some other place!) ', function () {
 
         assert.deepEqual(fromWhere('GP'), "Some other place!");
     });
