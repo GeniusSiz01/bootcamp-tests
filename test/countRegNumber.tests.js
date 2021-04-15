52cc2bc28bc3c1a34e8a97df91cde8a5e7a1b5d2
GeniusSiz01 <sizwe.msimango01@gmail.com>
2021-04-15 10:50:28 +0200

    my commit after a more descriptive story

--- test/countRegNumber.tests.js
+++ test/countRegNumber.tests.js
@@ -1,12 +1,12 @@
 describe('Test my countRegNumber function' , function() {
 
-    it('It should return "number" of registration numbers in the string ' , function(){
+    it('that takes in a string parameter that looks like this "CA 42665, AA 12 RT GP, CA 182736,CY 523519, CJ 812328" and returns the number of registration numbers in the string. ' , function(){
         
         assert.deepEqual(countRegNumber('DV 23 NB GP, CA 182736, CY 523519, CJ 812328'), 4);
 
     });
 
-    it('It should return "number" of registration numbers in the string ' , function(){
+    it('that takes in a string parameter that looks like this "CA 42665, AA 12 RT GP, CA 182736,CY 523519, CJ 812328" and returns the number of registration numbers in the string. ' , function(){
         
         assert.deepEqual(countRegNumber('DV 23 NB GP, CJ 812328'), 2);
 
